d147680845f81cedffe04e101b5ef8cff8ae467c
Muhamad Bayu Wilanda <muhamadbayuwilanda@protonmail.com>
2016-09-06 22:35:27 +0700

    Update koneksi.php

--- admin/koneksi.php
+++ admin/koneksi.php
@@ -1,5 +1,5 @@
 <?php
-mysql_connect("localhost","root","cobalutebak"); // isi sesuai host anda
+mysql_connect("localhost","",""); // isi sesuai host anda
 mysql_select_db("sekolah_bayu"); // nama database yang  saya buat tadi
 
 ?>
